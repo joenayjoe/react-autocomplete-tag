2ec3a282563a5fdb47e601ba3df69050c1339899
Junaid Alam <joenayjoe@gmail.com>
2021-01-30 20:48:53 +0100

    Updates

---
+++ precache-manifest.eb0adb738449feae0b159f388b8f771f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae909b6e17a9c1c47ddf082aff53068",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0b133fb43267552ea1d",
+    "url": "./static/css/main.d93954b7.chunk.css"
+  },
+  {
+    "revision": "058ce1d3e8387f791f9f",
+    "url": "./static/js/2.c00458bf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c00458bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b133fb43267552ea1d",
+    "url": "./static/js/main.903f5d46.chunk.js"
+  },
+  {
+    "revision": "9503924e378106c95c45",
+    "url": "./static/js/runtime-main.e755d8c8.js"
+  }
+]);
